33556c91c19f2f747adeaed378b9a342ecaabf27
Jhon930 <jhonnychamoli87@gmail.com>
2019-12-27 18:25:08 -0500

    adding new repository

---
+++ src/main/java/com/project/salaryadvance/SalaryAdvanceMicroserviceApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.salaryadvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryAdvanceMicroserviceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryAdvanceMicroserviceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/salaryadvance/SalaryAdvanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.salaryadvance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryAdvanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
